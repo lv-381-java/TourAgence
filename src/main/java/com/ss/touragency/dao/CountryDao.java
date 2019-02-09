ea27ba87cac81d0dd0eb4e5ae68fbae07426b307
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-10 00:07:35 +0200

    Override implementation methods for class CountryDao.java

--- src/main/java/com/ss/touragency/dao/CountryDao.java
+++ src/main/java/com/ss/touragency/dao/CountryDao.java
@@ -3,22 +3,44 @@ package com.ss.touragency.dao;
 import com.ss.touragency.dbConnection.DBConnection;
 import com.ss.touragency.entity.Country;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CountryDao implements ICrudDao<Country> {
 
     @Override
-    public void create(Country country) {
+    public void insert(Country country) {
+
+        String insertCountry = "INSERT INTO country(countryName) VALUES(?)";
+        Connection connection = DBConnection.getDbConnection();
+
+        if(connection != null){
+
+            PreparedStatement preparedStatement = null;
+
+            try {
+                preparedStatement = connection.prepareStatement(insertCountry);
+
+                preparedStatement.setString(1, country.getCountryName());
+                preparedStatement.execute();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }finally {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
 
     }
 
     @Override
-    public List<Country> getAll() throws SQLException {
+    public List<Country> selectAll() throws SQLException {
             List<Country> countryList = new ArrayList<>();
 
             String sql = "SELECT * FROM COUNTRY";
@@ -38,7 +60,7 @@ public class CountryDao implements ICrudDao<Country> {
     }
 
     @Override
-    public Country getById(int id) throws SQLException {
+    public Country selectById(int id) throws SQLException {
 
         String sql = "SELECT * FROM COUNTRY WHERE idCountry=" + "'" + id + "'";
         Statement statement = DBConnection.getDbConnection().createStatement();
@@ -59,19 +81,43 @@ public class CountryDao implements ICrudDao<Country> {
     }
 
     @Override
-    public void updateById(Country country) {
+    public void updateById(Country country, Long id) {
 
-        String sqlUpdate = "UPDATE countryName ";
+        String sqlUpdate = "UPDATE country SET countryName=?" + " WHERE idCountry=" + id + "";
+        PreparedStatement preparedStatement = null;
+        Connection connection = DBConnection.getDbConnection();
+        if (connection != null) {
 
-    }
+            try {
 
-    @Override
-    public void updateByName(Country country) {
+                preparedStatement = connection.prepareStatement(sqlUpdate);
 
+                preparedStatement.setString(1, country.getCountryName());
+
+                preparedStatement.executeUpdate();
+
+                connection.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void deleteById(Long id) {
 
+        String deleteQuery = "DELETE FROM country WHERE idCountry=" + id + "";
+
+        Statement statement;
+        try {
+            statement = DBConnection.getDbConnection().createStatement();
+
+            statement.executeUpdate(deleteQuery);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 }
