ff92855fb7e1e9424e462df56160fb4d69c2dc00
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-10 00:32:57 +0200

    Create entity class Visa.java

---
+++ src/main/java/com/ss/touragency/entity/Visa.java
@@ -0,0 +1,49 @@
+package com.ss.touragency.entity;
+
+public class Visa {
+
+    private Long id;
+    private Client client;
+    private Country country;
+
+    public Visa() {}
+
+    public Visa(Long id, Client client, Country country) {
+        this.id = id;
+        this.client = client;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Visa{" +
+                "id=" + id +
+                ", client=" + client +
+                ", country=" + country +
+                '}';
+    }
+}
