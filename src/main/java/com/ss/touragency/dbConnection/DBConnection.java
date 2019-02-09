62e22bbc76c1a22e2b2ae3449c0dbc7a35323de9
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-10 00:03:30 +0200

    Create architecture for class DBConnection.java

--- src/main/java/com/ss/touragency/dbConnection/DBConnection.java
+++ src/main/java/com/ss/touragency/dbConnection/DBConnection.java
@@ -15,7 +15,7 @@ public class DBConnection {
     private static final String DB_USER = "onlyVone";
     private static final String DB_PASSWORD = "kxfat92a";
 
-    public static Connection getDbConnection() throws SQLException {
+    public static Connection getDbConnection() {
 
         if(connection == null){
             connection = initConnection();
@@ -24,22 +24,28 @@ public class DBConnection {
         return connection;
     }
 
-    public void closeConnection() throws SQLException {
+    public void closeConnection() {
 
         if(connection == null){
             return;
         }
-        connection.close();
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static Connection initConnection() throws SQLException {
+    private static Connection initConnection() {
 
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
 
     }
-
-//    public static Connection getDbConnection() throws SQLException {
-//        return DriverManager.getConnection(DbProperties.url, DbProperties.user, DbProperties.password);
-//    }
 
 }
