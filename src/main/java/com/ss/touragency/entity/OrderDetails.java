9f9d8f38cef6fcc2c5142adeb9bd94b350643861
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-09 15:53:20 +0200

    Create class OrderDetails.java

--- src/main/java/com/ss/touragency/entity/OrderDetails.java
+++ src/main/java/com/ss/touragency/entity/OrderDetails.java
@@ -1,71 +1,71 @@
-//package com.ss.touragency.entity;
-//
-//public class OrderDetails {
-//
-//    private int id;
-//    private Client client;
-//    private Hotel hotel;
-//    private String beginDate;
-//    private String endDate;
-//
-//    public OrderDetails(int id, Client client, Hotel hotel, String beginDate, String endDate) {
-//        this.id = id;
-//        this.client = client;
-//        this.hotel = hotel;
-//        this.beginDate = beginDate;
-//        this.endDate = endDate;
-//    }
-//
-//    public OrderDetails() {}
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public Client getClient() {
-//        return client;
-//    }
-//
-//    public void setClient(Client client) {
-//        this.client = client;
-//    }
-//
-//    public Hotel getHotel() {
-//        return hotel;
-//    }
-//
-//    public void setHotel(Hotel hotel) {
-//        this.hotel = hotel;
-//    }
-//
-//    public String getBeginDate() {
-//        return beginDate;
-//    }
-//
-//    public void setBeginDate(String beginDate) {
-//        this.beginDate = beginDate;
-//    }
-//
-//    public String getEndDate() {
-//        return endDate;
-//    }
-//
-//    public void setEndDate(String endDate) {
-//        this.endDate = endDate;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "OrderDetails{" +
-//                "id=" + id +
-//                ", client=" + client +
-//                ", hotel=" + hotel +
-//                ", beginDate='" + beginDate + '\'' +
-//                ", endDate='" + endDate + '\'' +
-//                '}';
-//    }
-//}
+package com.ss.touragency.entity;
+
+public class OrderDetails {
+
+    private int id;
+    private Client client;
+    private Hotel hotel;
+    private String beginDate;
+    private String endDate;
+
+    public OrderDetails(int id, Client client, Hotel hotel, String beginDate, String endDate) {
+        this.id = id;
+        this.client = client;
+        this.hotel = hotel;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+    }
+
+    public OrderDetails() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public String getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(String beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetails{" +
+                "id=" + id +
+                ", client=" + client +
+                ", hotel=" + hotel +
+                ", beginDate='" + beginDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+}
