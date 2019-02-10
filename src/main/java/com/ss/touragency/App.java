56b1849eaa4bcb20c7b0087381d78955a3f6519d
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-11 00:23:09 +0200

    Check the work of the CityService.java class in App.java

--- src/main/java/com/ss/touragency/App.java
+++ src/main/java/com/ss/touragency/App.java
@@ -5,16 +5,19 @@ import com.ss.touragency.dao.CountryDao;
 import com.ss.touragency.dbConnection.DBConnection;
 import com.ss.touragency.entity.City;
 import com.ss.touragency.entity.Country;
+import com.ss.touragency.service.CityService;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
 
     public static void main( String[] args ) throws SQLException {
 
         CountryDao c = new CountryDao();
-        Country country = new Country("CountryName2");
+//        Country country = new Country("CountryName2");
 
 //        System.out.println(c.selectById((long)5));
 //        c.deleteById((long) 10);
@@ -30,13 +33,24 @@ public class App {
 //        c.deleteById(9l);
 //        System.out.println(c.selectAll());
 
-        CityDao city = new CityDao();
-        City city2 = new City("Neapol",c.selectById(5l));
-        City city3 = new City("Bordo",c.selectById(4l));
+//        CityDao city = new CityDao();
+//        City city2 = new City("Neapol",c.selectById(5l));
+//        City city3 = new City("Bordo",c.selectById(4l));
 //        city.insert(city2);
 //        System.out.println(city.selectById(3L));
 //        city.updateById(city3,9L);
 //        city.deleteById(10l);
+
+//        CityService cityService = new CityService();
+//        Map<Country, List<City>> map = cityService.selectAllCountrisWithCities();
+//
+//        for(Map.Entry<Country, List<City>> entry : map.entrySet()){
+//
+//            Country cou = entry.getKey();
+//            List<City> cityList = entry.getValue();
+//            System.out.println(cou.getCountryName() + " : " + cityList);
+//
+//        }
 
     }
 }
