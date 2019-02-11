cc8c5768b121be4cf0c92db0a02bb27157938f96
Arsen <arsenemarkiv@gmail.com>
2019-02-11 21:02:04 +0200

    order deteils create

--- src/main/java/com/ss/touragency/service/OrderDetailsService.java
+++ src/main/java/com/ss/touragency/service/OrderDetailsService.java
@@ -2,25 +2,41 @@ package com.ss.touragency.service;
 
 import com.ss.touragency.constants.Attribute;
 import com.ss.touragency.dao.OrderDetailsDao;
+import com.ss.touragency.entity.Client;
+import com.ss.touragency.entity.Hotel;
 import com.ss.touragency.entity.OrderDetails;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class OrderDetailsService {
 
-    // FIXME: finish
-    public boolean createOrder(HttpServletRequest request) {
-        boolean status = false;
+    // TODO: finish. assigned to Arsen
+    public boolean createOrder(HttpServletRequest request) throws ParseException {
 
-        String client = (String) request.getSession().getAttribute(Attribute.CLIENT);
-        String hotel = (String) request.getSession().getAttribute(Attribute.HOTEL);
+        boolean status = false;
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+//        String client = (String) request.getSession().getAttribute(Attribute.CLIENT);
+//        String hotel = (String) request.getSession().getAttribute(Attribute.HOTEL);
         String beginDate = (String) request.getSession().getAttribute(Attribute.BEGIN_DATE);
         String endDate = (String) request.getSession().getAttribute(Attribute.END_DATE);
 
+        Hotel hotel = new Hotel();
+        Client client = new Client();
+
         if (client != null && hotel != null && beginDate != null && endDate != null) {
+
             OrderDetailsDao orderDetailsDao = new OrderDetailsDao();
-//            OrderDetails orderDetails = new OrderDetails(-1L, client, hotel, beginDate, endDate);
+            Date begin = (Date) formatter.parse(beginDate);
+            Date end = (Date) formatter.parse(endDate);
+
+            OrderDetails orderDetails = new OrderDetails(-1L, client, hotel, begin, end);
+            orderDetailsDao.insert(orderDetails);
+            status = true;
+
         }
         return status;
     }
