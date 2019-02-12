acc8d43e2fe15dd317b30384df831e213abd7e38
Arsen <arsenemarkiv@gmail.com>
2019-02-12 14:08:30 +0200

    path for create servlet

--- src/main/java/com/ss/touragency/controller/country/CountryServlet.java
+++ src/main/java/com/ss/touragency/controller/country/CountryServlet.java
@@ -1,7 +1,9 @@
 package com.ss.touragency.controller.country;
 
+import com.ss.touragency.constants.PathToPage;
+
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name = "")
+@WebServlet(PathToPage.CREATE_COUNTRY)
 public class CountryServlet {
 }
