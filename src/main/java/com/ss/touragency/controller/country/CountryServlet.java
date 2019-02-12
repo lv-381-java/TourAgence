491c893f404b16838754476e977e4c886695243d
Nazar <nazarmavko@gmail.com>
2019-02-12 15:34:13 +0200

    first servlet

--- src/main/java/com/ss/touragency/controller/country/CountryServlet.java
+++ src/main/java/com/ss/touragency/controller/country/CountryServlet.java
@@ -2,9 +2,23 @@ package com.ss.touragency.controller.country;
 
 import com.ss.touragency.constants.PathToPage;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(PathToPage.CREATE_COUNTRY)
 
-public class CountryServlet {
+public class CountryServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/home.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
