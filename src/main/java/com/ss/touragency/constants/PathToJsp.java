33f67e2f745e344cecfa00a9d2c6f8294594b262
Arsen <arsenemarkiv@gmail.com>
2019-02-12 16:59:28 +0200

    city servlet

--- src/main/java/com/ss/touragency/constants/PathToJsp.java
+++ src/main/java/com/ss/touragency/constants/PathToJsp.java
@@ -3,6 +3,8 @@ package com.ss.touragency.constants;
 public class PathToJsp {
 
     public static final String HOME_JSP = "/WEB-INF/home.jsp";
-
+    public static final String HOTEL_JSP = "/WEB-INF/hotel.jsp";
+    public static final String USER_JSP = "/WEB-INF/user.jsp";
+    public static final String LOGIN_JSP = "/WEB-INF/login.jsp";
 
 }
--- src/main/java/com/ss/touragency/controller/country/CityServlet.java
+++ src/main/java/com/ss/touragency/controller/country/CityServlet.java
@@ -6,6 +6,7 @@ import com.ss.touragency.constants.PathToJsp;
 import com.ss.touragency.constants.PathToPage;
 import com.ss.touragency.util.Context;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +15,7 @@ import java.io.IOException;
 @WebServlet(PathToPage.CITY_INFO)
 public class CityServlet {
 
-    void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         //TODO: login check
         if (request.getSession().getAttribute(Attribute.CITY) == null) {
@@ -23,14 +24,26 @@ public class CityServlet {
                 response.sendRedirect(PathToPage.CREATE_CITY);
             } else {
                 request.setAttribute(Attribute.ERROR, "Something went wrong! Please try again");
-                request.getRequestDispatcher(PathToJsp.HOME_JSP);
+                request.getRequestDispatcher(PathToJsp.HOME_JSP).forward(request, response);
             }
 
         } else {
             request.setAttribute(Attribute.ERROR, "You are not login!");
-//            request.getRequestDispatcher(PathToJsp.LOGIN_JSP);
+//            request.getRequestDispatcher(PathToJsp.LOGIN_JSP).forward(request, response);
         }
     }
 
+    void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //TODO: login check
+        if (request.getSession().getAttribute(Attribute.CITY_ID) == null ){
+//            request.getRequestDispatcher()
+        }
+        else {
+            request.setAttribute(Attribute.ERROR, "You are not login!");
+            request.getRequestDispatcher(PathToJsp.LOGIN_JSP).forward(request, response);
+        }
+
+
+    }
 }
