b77b729efef5ed395bb7bd311d2a9dd79ceabda4
Arsen <arsenemarkiv@gmail.com>
2019-02-13 12:19:29 +0200

    merge country info servlet

--- src/main/java/com/ss/touragency/controller/country/CountryInfoServlet.java
+++ src/main/java/com/ss/touragency/controller/country/CountryInfoServlet.java
@@ -3,6 +3,7 @@ package com.ss.touragency.controller.country;
 import com.ss.touragency.constants.PathToPage;
 import com.ss.touragency.dao.CountryDao;
 import com.ss.touragency.entity.Country;
+import com.ss.touragency.service.CountryService;
 import com.ss.touragency.util.Context;
 
 import javax.servlet.ServletException;
@@ -11,21 +12,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(PathToPage.COUNTRY_INFO)
-public class CountryServlet extends HttpServlet {
+public class CountryInfoServlet extends HttpServlet {
+    public static final String
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String country = Context.getInstance().getCountryService().getCountry(request).getCountryName();
+        request.setAttribute("list", country);
 
-        if(Context.getInstance().getCountryService().getCountry(req)){
-
-        }
-        CountryDao countryDao = new CountryDao();
-        List<Country> countryList = countryDao.selectAll();
-        req.setAttribute("list", countryList);
-        req.getRequestDispatcher("WEB-INF/views/country.jsp").forward(req, resp);
+        request.getRequestDispatcher("WEB-INF/views/country.jsp").forward(request, response);
 
     }
 
